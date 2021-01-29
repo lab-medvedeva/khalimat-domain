70e1b12d5962696c32a19cbdae0602992dfc59a7
khalimat-bg <khalimat.murtazalieva@bostongene.com>
2021-01-29 22:04:09 +0300

    bag fix

--- plot_Tropsha.py
+++ plot_Tropsha.py
@@ -6,27 +6,26 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 res_path= '/home/kmurtazalieva/Downloads/scams/Results'
-METRICS = ['AUC_LB', 'AUC', 'AUC_UB', 'Accuracy', 'F1_test', 'MCC_test']
+METRICS = ['AUC_LB', 'AUC', 'AUC_UB', 'Accuracy', 'F1', 'MCC']
 
 to_plot = {
-    # 'RF_N_SP1_SS': 'RF, No sampling, SCAMS_balanced_with_positive.csv, Scaffold splitter',
-    #        'RF_ADASYN_SF_SS': 'RF, ADASYN, SCAMS_filtered.csv, Scaffold splitter',
-    #        'RF_SMOTE_SF_SS': 'RF, SMOTE, SCAMS_filtered.csv, Scaffold splitter',
-    #        'RF_N_SP1_TT1': 'RF, No sampling, SCAMS_filtered.csv, Scaffold splitter',
-    #        'RF_CNN_SF_SS': 'RF, CondensedNearestNeighbour, SCAMS_filtered.csv, Scaffold splitter',
-    #        'RF_SMOTE_SP2_SS': 'RF, SMOTE, SCAMS_added_positives_653_1043.csv, Scaffold splitter',
-    #        'RF_ADASYN_SP2_SS': 'RF, ADASYN, SCAMS_added_positives_653_1043.csv, Scaffold splitter',
-    #        'RF_CNN_SP2_SS': 'RF, CondensedNearest., SCAMS_added_positives_653_1043.csv, Scaffold splitter'
-    #        'RF_IHT_SP2_SS': 'RF, InstanceHardness., SCAMS_added_positives_653_1043.csv, Scaffold splitter'
-    'RF_IHT_SF_SS': 'RF, InstanceHardness., SCAMS_filtered.csv, Scaffold splitter'
+           # 'RF_N_SP1_SS': 'RF, No sampling, SCAMS_balanced_with_positive.csv, Scaffold splitter',
+           'RF_ADASYN_SF_SS': 'RF, ADASYN, SCAMS_filtered.csv, Scaffold splitter',
+           'RF_SMOTE_SF_SS': 'RF, SMOTE, SCAMS_filtered.csv, Scaffold splitter',
+           'RF_N_SP1_TT1': 'RF, No sampling, SCAMS_filtered.csv, Scaffold splitter',
+           'RF_CNN_SF_SS': 'RF, CondensedNearestNeighbour, SCAMS_filtered.csv, Scaffold splitter',
+           'RF_SMOTE_SP2_SS': 'RF, SMOTE, SCAMS_added_positives_653_1043.csv, Scaffold splitter',
+           'RF_ADASYN_SP2_SS': 'RF, ADASYN, SCAMS_added_positives_653_1043.csv, Scaffold splitter',
+           'RF_CNN_SP2_SS': 'RF, CondensedNearest., SCAMS_added_positives_653_1043.csv, Scaffold splitter',
+           'RF_IHT_SP2_SS': 'RF, InstanceHardness., SCAMS_added_positives_653_1043.csv, Scaffold splitter',
+            'RF_IHT_SF_SS': 'RF, InstanceHardness., SCAMS_filtered.csv, Scaffold splitter'
            }
-# RF_SMOTE_SP2_SS': 'RandomForestClassifier, SMOTE, SCAMS_added_positives_653_1043.csv, Scaffold splitter'
 
 def make_plot(paret_pat, dir, title):
     path= Path(res_path)/ dir/ 't-test_stats.csv'
     t_test = pd.read_csv(path)
     t_test = t_test.set_index('Metrics')
-    print(t_test)
+    print(t_test[['Mean non AL', 'Mean AL']])
     AL = t_test['Mean AL'].loc[METRICS]
     non_AL = t_test['Mean non AL'].loc[METRICS]
     # Make adaptive max score
